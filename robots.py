a90ac9b6b5a13163f3cecf0ef1dd35d854b87d8b
Conquer <61772700+Never-Say-Give-Up@users.noreply.github.com>
2020-04-27 12:02:45 +0800

    【领航预览版】增加新功能！！！
    这是一个Update

--- robots.py
+++ robots.py
@@ -2,24 +2,25 @@
 # coding: utf-8
 
 # In[14]:
-import re
-
 try:
     from selenium import webdriver
     from selenium.webdriver.chrome.options import Options
     from bs4 import BeautifulSoup
     import requests
+    import re
 except:
     print("库不完善，请检查依赖安装情况：")
     print("1. selenium")
     print("2. requests")
     print("3. bs4")
+    print("4. re")
     exit(0)
     
 
 #Selenium库 
-## initChrome 函数：无窗口模式
-## 初始化浏览器对象
+
+## initChrome 函数
+## 无窗口初始化Chrome模式
 ## 返回值: (对象)webdriver浏览器
 def initChrome():
     oc = Options()
@@ -32,19 +33,33 @@ def initChrome():
         print("缺少Chrome驱动，或Chrome安装不正确！")
         exit(0)
 
+        
+## getVal函数
+## 获取html id属性的文本内容
+## 传入值：(对象)浏览器，(字符串)类型<id、name>，(字符串)属性值
+## 返回值：(字符串)文本内容
+def getVal(bro,types,val):
+    if (types == 'id'):
+        return str(bro.find_element_by_id(val).text)
+    elif (types == 'name'):
+        return str(bro.find_element_by_bame(val).text)
 
 #requests库
+
+# *** 基础函数 ***
 
 ## getHtml 函数
 ## 获取网页源代码
 ## 返回值: (字符串)html源码
-def getHtml(url):
-    r = requests.request("GET",url)
+def getHtml(htmlurl):
+    r = requests.request("GET",htmlurl)
     if (r.status_code != 200):
         print("网络连接错误！")
         exit(0)
     r.encoding = r.apparent_encoding
     return r.text
+
+# *** 基于基础函数的功能函数 ***
 
 ## getTitle 函数
 ## 获取页面标题
@@ -62,19 +77,38 @@ def getResource(text,types):
 
 ## download 函数
 ## 下载数据到本地
+## 传入值: [列表](字符串)资源url地址 or (字符串)资源url地址
 ## 返回值: (整型数据)成功(1)、失败(0)
-def download(url,name):
-    r = requests.get(url)
-    if (r.status_code != 200):
-        print("网络连接错误！")
-        exit(0)
-    with open(name,'wb') as file:
-        try:
-            file.write(r.content)
-            print("下载成功！")
-            print("文件名为：" + name)
-            return 1
-        except:
-            print("文件写入失败！")
-            return 0
-
+def Download(url,name):
+    if (type(url) == list):
+        num = 0
+        for i in url:
+            r = requests.get(i)
+            if (r.status_code != 200):
+                print("网络连接错误！")
+                exit(0)
+            with open(str(num) + name,'wb') as file:
+                try:
+                    num += 1
+                    file.write(r.content)
+                    print("下载成功！")
+                    print("文件名为：" + str(num) + name)
+                except:
+                    print("文件写入失败！")
+                    return 0
+        return 1
+    elif (type(url) == str):
+        r = requests.get(url)
+        if (r.status_code != 200):
+            print("网络连接错误！")
+            exit(0)
+        with open(name,'wb') as file:
+            try:
+                file.write(r.content)
+                print("下载成功！")
+                print("文件名为：" + name)
+                return 1
+            except:
+                print("文件写入失败！")
+                return 0
+    
